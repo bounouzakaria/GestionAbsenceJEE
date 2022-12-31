5abd217a2c3eef06a6e25e1d8f871d6a5a9144c9
AhmedBr2022 <118344327+AhmedBr2022@users.noreply.github.com>
2022-12-31 01:09:28 +0100

    Creation Page Authentification+IndexSeance et ConnectionDB

---
+++ src/main/java/seances/ConnectionDB.java
@@ -0,0 +1,46 @@
+package seances;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import javax.swing.JOptionPane;
+
+import web.dbConnection;
+
+public class ConnectionDB {
+	Connection db;
+	
+	public ConnectionDB()
+	{
+		
+	}
+	
+	public Connection init() {
+		if(this.db == null) {
+			this.db = dbConnection.Cnx();
+		}
+		return this.db;
+	}
+	public static Connection Cnx()
+	{
+		  String url = "jdbc:mysql://localhost:3306/gestionabsence?characterEncoding=utf8";
+		  String utilisateur = "root";
+		  String motDePasse = "";
+		  try {
+			  Class.forName("com.mysql.jdbc.Driver");
+			  Connection cnx = DriverManager.getConnection( url, utilisateur, motDePasse );
+			  return cnx;
+			  
+		  }catch (Exception e) {
+			  
+			  JOptionPane.showMessageDialog(null, e);
+			  return null;
+			  
+		  }
+		
+	}
+
+	
+	
+
+}
